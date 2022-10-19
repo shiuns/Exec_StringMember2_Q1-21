cc198bc9633f434a403b2b82dab825992c433510
ispan <ispan@DESKTOP-PAKSETB>
2022-10-19 15:45:37 +0800

    加入專案檔案。

---
+++ [(Exec_StringMember_Q1)-21 謝孟勳/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace __Exec_StringMember_Q1__21_謝孟勳
+{
+    //將" abc 左右的空白刪除"
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "  abc  ";
+            Console.WriteLine(value.Trim());
+        }
+    }
+}
